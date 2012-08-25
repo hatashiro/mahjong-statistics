803f18e07f1910391c8dbfec72ebd74c4e0d26ec
noraesae <noraesae@yuiazu.net>
2012-08-25 23:04:34 +0900

    Use south to migrate database models.
    Committed by noraesae.

--- mahjong/mahjong/settings.py
+++ mahjong/mahjong/settings.py
@@ -123,6 +123,9 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
+    # south
+    'south',
+
     # custom apps
     'auths',
     'pages',
